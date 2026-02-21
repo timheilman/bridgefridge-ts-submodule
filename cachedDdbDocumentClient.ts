71d489d7298ea322f81e1549cceae32f0ae74cbc
Tim Heilman <tdh@stanfordalumni.org>
2026-02-21 07:40:02 -0800

    refactor: scor-527 wrap cached ddb client in doc client and rename its ts file

--- cachedDdbDocumentClient.ts
+++ cachedDdbDocumentClient.ts
@@ -1,22 +1,24 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 import { cachedAwsSdkV3Client } from "./cachedAwsSdkV3Client";
 
 const profileDict: Record<string, Record<string, DynamoDBClient>> = {};
 const envDict: Record<string, DynamoDBClient> = {};
 
-export const cachedDynamoDbClient = ({
+export const cachedDdbDocumentClient = ({
   awsRegion,
   profile,
 }: {
   awsRegion: string;
   profile: string | null;
-}): DynamoDBClient => {
-  return cachedAwsSdkV3Client<DynamoDBClient>(
+}): DynamoDBDocumentClient => {
+  const raw = cachedAwsSdkV3Client<DynamoDBClient>(
     DynamoDBClient,
     awsRegion,
     profile,
     profileDict,
     envDict,
   );
+  return DynamoDBDocumentClient.from(raw);
 };
