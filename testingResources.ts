40c6adfdc659b9b45424b99c39deeddb220d073d
Tim Heilman <tdh@stanfordalumni.org>
2026-03-06 07:12:22 -0800

    scor-528: share the list of cdk-seeded clubHuman display names across repos

---
+++ testingResources.ts
@@ -0,0 +1,14 @@
+export const CDK_SEEDED_CLUB_HUMAN_DISPLAY_NAMES = new Set([
+  "Nancy",
+  "Norman",
+  "Natalie",
+  "Sam",
+  "Susan",
+  "Steve",
+  "Emily",
+  "Edward",
+  "Emma",
+  "William",
+  "Walter",
+  "Wendy",
+]);
