280004781f03a1452988bfa3251183ed655ffb43
Tim Heilman <tdh@stanfordalumni.org>
2026-03-13 11:57:37 -0700

    test: scor-532 add test device name for automated testing

--- testingResources.ts
+++ testingResources.ts
@@ -1,3 +1,6 @@
+export const CDK_SEEDED_CLUB_DEVICE_NAME =
+  "cdk-test-device with long name to test truncation";
+
 export const CDK_SEEDED_CLUB_HUMAN_DISPLAY_NAMES = new Set([
   "Nancy",
   "Norman",
