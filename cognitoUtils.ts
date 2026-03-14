3c94a08f60e3cd6143a2458977851a5d4e4fa41d
Tim Heilman <tdh@stanfordalumni.org>
2026-03-14 08:06:56 -0700

    test: scor-532 add nullableCognitoUser to cognitoUtils

--- cognitoUtils.ts
+++ cognitoUtils.ts
@@ -1,19 +1,32 @@
 import {
   AdminGetUserCommand,
   CognitoIdentityProviderClient,
+  UserNotFoundException,
 } from "@aws-sdk/client-cognito-identity-provider";
+interface CognitoUserParams {
+  cognitoIdpClient: CognitoIdentityProviderClient;
+  userPoolId: string;
+  usernameOrEmail: string;
+}
+
 export const cognitoUser = ({
   userPoolId,
   cognitoIdpClient,
   usernameOrEmail,
-}: {
-  cognitoIdpClient: CognitoIdentityProviderClient;
-  userPoolId: string;
-  usernameOrEmail: string;
-}) =>
+}: CognitoUserParams) =>
   cognitoIdpClient.send(
     new AdminGetUserCommand({
       UserPoolId: userPoolId,
       Username: usernameOrEmail,
     }),
   );
+export const nullableCognitoUser = async (params: CognitoUserParams) => {
+  try {
+    return await cognitoUser(params);
+  } catch (e) {
+    if (e instanceof UserNotFoundException) {
+      return null;
+    }
+    throw e;
+  }
+};
